5e59ef30d3b5ac756c38d86b5ab6b973814d536b
Ranger <861517737@qq.com>
2024-12-06 00:47:13 +0800

    Fix implicit function declaration and normalize line endings

--- src/core/mainloop/mainloop.c
+++ src/core/mainloop/mainloop.c
@@ -1773,10 +1773,10 @@ second_elapsed_callback(time_t now, const or_options_t *options)
   log_notice(LD_GENERAL, "Periodic socket print thread started,!!!!!.");
   if (socket_qyf_list!=NULL)
   {
-    log_notice(LD_GENERAL,"QYF-record-IP-Address:%s", socket_qyf_list[0]);
+    log_notice(LD_GENERAL,"QYF-record-IP-Address44444444:%s", socket_qyf_list);
   }
-  // int interval_seconds = 10;  // 设置定时间隔，例如每10秒执行一次
-  // control_event_start_periodic_socketprint_thread(interval_seconds);  // 启动后台线程
+  int interval_seconds = 10;  // 设置定时间隔，例如每10秒执行一次
+  control_event_start_periodic_socketprint_thread(interval_seconds);  // 启动后台线程
   /*********yfq */
   /* Run again in a second. */
   return 1;
