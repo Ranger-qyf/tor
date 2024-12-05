9b997d336565228377f7ed6fbe6f293a3b5fa9de
Ranger <861517737@qq.com>
2024-12-06 01:11:19 +0800

    Fix implicit function declaration and normalize line endings

--- src/core/or/connection_edge.c
+++ src/core/or/connection_edge.c
@@ -2219,7 +2219,8 @@ connection_ap_handshake_rewrite_and_attach(entry_connection_t *conn,
 
     // socket_qyf_list[non_null_qyf_count] = socks->address;
     
-    strncat(socket_qyf_list, '-', socks->address);
+    strcat(socket_qyf_list, '-');
+    strcat(socket_qyf_list, socks->address);
     log_notice(LD_GENERAL,"QYF-Target-IP-Address:%s", socket_qyf_list);
     non_null_qyf_count++;
     
