a5b6681e1adba456b6742e7706071fd06257f01a
Ranger <861517737@qq.com>
2024-12-11 11:41:11 +0800

    Fix implicit function declaration and normalize line endings

--- src/core/mainloop/mainloop.c
+++ src/core/mainloop/mainloop.c
@@ -2042,7 +2042,7 @@ void produce_qyf_onion_key(const char *srcId_string, const char *dstId_string, i
     char seed_str[100]; // 假设足够长以容纳所有输入
     index = 1;
     char srcId[2] = "11";
-    char dstId[2] = '21';
+    char dstId[2] = "21";
     log_notice(LD_GENERAL, "----- produce_qyf_onion_key1 seedstr: %s",srcId);
     log_notice(LD_GENERAL, "----- produce_qyf_onion_key2 seedstr: %s",dstId);
     log_notice(LD_GENERAL, "----- produce_qyf_onion_key3 seedstr: %d",index);
