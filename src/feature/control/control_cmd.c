addeb7324f969d1347f3a5223fbd5f20daa22d7f
Ranger <861517737@qq.com>
2024-12-12 08:37:25 +0800

    Fix implicit function declaration and normalize line endings

--- src/feature/control/control_cmd.c
+++ src/feature/control/control_cmd.c
@@ -1882,6 +1882,43 @@ static const control_cmd_syntax_t add_onion_syntax = {
   .allowed_keywords = add_onion_keywords
 };
 
+void print_smartlist(const struct smartlist_t *sl) {
+    if (!sl) {
+        printf("Smartlist is NULL.\n");
+        return;
+    }
+
+    for (int i = 0; i < smartlist_len(sl); ++i) {
+        const char *item = smartlist_get(sl, i);
+        if (item) {
+            log_notice(LD_GENERAL,"----- smartlist is %s  ------------", item);
+            //printf("  Arg %d: %s\n", i + 1, item);
+        } else {
+            log_notice(LD_GENERAL,"----- smartlist is NULL  ------------");
+        }
+    }
+}
+
+// 打印 config_line_t 中的内容
+void print_config_lines(const control_cmd_args_t *args) {
+  const config_line_t *arg;
+    for (arg = args->kwargs; arg; arg = arg->next) 
+    {
+      log_notice(LD_GENERAL,"----- onfig_lines KEY is %s  VALUE is %s ------------", arg->key, arg->value);
+    }
+    // if (!cl) {
+    //     printf("Config lines are NULL.\n");
+    //     return;
+    // }
+
+    // for (const struct config_line_t *line = cl; line && line->key; ++line) {
+    //     printf("  Key: %s, Value: %s\n", line->key, line->value ? line->value : "(null)");
+    // }
+}
+
+
+
+
 /** Called when we get a ADD_ONION command; parse the body, and set up
  * the new ephemeral Onion Service. */
 static int
@@ -1904,7 +1941,11 @@ handle_control_add_onion(control_connection_t *conn,
   const config_line_t *arg;
   int number_of_onions = 0;
   int sum_of_replica = 0;
-  
+  //printf("Positional Arguments:\n");
+  print_smartlist(args->args);
+
+  //printf("Keyword Arguments:\n");
+  print_config_lines(args);
   for (arg = args->kwargs; arg; arg = arg->next) {
   /************yfq */
     if (!strcasecmp(arg->key, "SumOfReplica")) {
