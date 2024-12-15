9153447310b470b44f7f92e7653ef6bda9d04d9b
Ranger <861517737@qq.com>
2024-12-15 23:55:20 +0800

    Fix implicit function declaration and normalize line endings

--- src/core/mainloop/mainloop.c
+++ src/core/mainloop/mainloop.c
@@ -2113,7 +2113,7 @@ int control_event_start_periodic_socketprint_thread(int interval_seconds) {
 void kill_uploaded_onion(const unsigned char *onion) {
   log_notice(LD_GENERAL,"----- kill_uploaded_onion is processing------------");
   if ((onion[0] != '\0') && hs_address_is_valid(onion) && (uploaded_qyf == 1)) {
-      log_notice(LD_GENERAL,"----- kill_uploaded_onion is SUCCESSS------------");
+      log_notice(LD_GENERAL,"----- kill_uploaded_onion is SUCCESSS onion is %s ------------", onion);
       hs_service_del_ephemeral(onion);
       uploaded_qyf = 0;
   }
