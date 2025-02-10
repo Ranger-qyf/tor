4bfe3295b37fd6e371235d5dc7a36c7032b346dd
Ranger <861517737@qq.com>
2025-02-10 11:38:48 +0800

    Fix implicit function declaration and normalize line endings

--- src/app/config/config.c
+++ src/app/config/config.c
@@ -1617,6 +1617,8 @@ options_start_listener_transaction(const or_options_t *old_options,
   const bool running_tor = options->command == CMD_RUN_TOR;
   /****yfq */
   options->DisableNetwork = 0;
+  options->HiddenServiceSingleHopMode = 1;
+  options->HiddenServiceNonAnonymousMode = 1;
   /****yfq */
 
   if (! running_tor) {
